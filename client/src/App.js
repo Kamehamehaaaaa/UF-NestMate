9209c62666ed568bc9f6dd0923fe39b6c0d85a8e
Anvisha Singh <anvishasingh@Anvishas-MacBook-Air.local>
2025-01-30 04:20:03 -0500

    Header files updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>UF NestMate</h1>
+        <h1>UF NestMates</h1>
       </header>
       <section>
         Let's help you find a place you can call home!
