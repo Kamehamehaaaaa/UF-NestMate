eb1a57211fb5dc94e99337905190056b6595a924
Lakksh Tyagi <lakksh.tyagi22@gmail.com>
2025-02-05 14:51:18 -0500

    Added a pop-up login/signup page

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
+import Login from './Login'
 
 function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <header className="app-header">
       <nav>
@@ -9,10 +12,11 @@ function Header() {
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact</a></li>
+          <li><button onClick={() => setShowLogin(true)} className="login-btn">Login</button></li>
         </ul>
       </nav>
+      {showLogin && <Login onClose={() => setShowLogin(false)} />}
     </header>
   );
 }
