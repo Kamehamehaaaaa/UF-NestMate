cb7458cfb583995fdcc14e07fd6b1f18263be902
Rohit Bogulla <rohitbogulla@Rohits-MacBook-Air.local>
2025-03-03 18:17:34 -0500

    unit tests for housing apis and json payload updated in housing apis

--- apis/router/housingHandler_test.go
+++ apis/router/housingHandler_test.go
@@ -5,7 +5,6 @@ import (
 	"apis/housing"
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -39,7 +38,7 @@ func TestAddHousingHandler(t *testing.T) {
 
 	dataEntry, exists := data.Housings["1"]
 	if !exists || dataEntry.Name != "Apartment1" || dataEntry.Address != "Address" {
-		t.Errorf("housing data was not updated correctly")
+		t.Errorf("housing data was not added correctly")
 	}
 }
 
@@ -90,8 +89,6 @@ func TestGetHousingHandler(t *testing.T) {
 		t.Errorf("housing data retrived correctly")
 	}
 
-	fmt.Println(bodyJson["1"])
-
 	_, exists := bodyJson["1"]
 	if !exists {
 		t.Errorf("housing data missing entries")
@@ -102,4 +99,83 @@ func TestGetHousingHandler(t *testing.T) {
 		t.Errorf("housing data missing entries")
 	}
 
+}
+
+func TestUpdateHousingHandler(t *testing.T) {
+	data.Housings = make(map[string]housing.Housing)
+
+	h1 := housing.Housing{
+		ID:          "1",
+		Name:        "Apartment1",
+		Address:     "Address1",
+		Vacancy:     100,
+		Rating:      4.3,
+		Description: "A nice place to stay.",
+	}
+
+	data.Housings["1"] = h1
+
+	housingPayload := housing.HousingPayload{
+		ID:          "1",
+		Name:        "Apartment1",
+		Address:     "Address",
+		Vacancy:     60,
+		Rating:      4.5,
+		Description: "A nice place to stay.",
+	}
+	payloadBytes, _ := json.Marshal(housingPayload)
+
+	req, err := http.NewRequest("PUT", "/api/housing/update", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(UpdateHousingHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("expected status %d, got %d", http.StatusOK, status)
+	}
+
+	dataEntry, exists := data.Housings["1"]
+	if !exists || dataEntry.Name != "Apartment1" || dataEntry.Rating != 4.5 || dataEntry.Vacancy != 60 {
+		t.Errorf("housing data was not updated correctly")
+	}
+}
+
+func TestDeleteHousingHandler(t *testing.T) {
+	data.Housings = make(map[string]housing.Housing)
+
+	h1 := housing.Housing{
+		ID:          "1",
+		Name:        "Apartment1",
+		Address:     "Address1",
+		Vacancy:     100,
+		Rating:      4.3,
+		Description: "A nice place to stay.",
+	}
+
+	data.Housings["1"] = h1
+
+	housingPayload := housing.HousingPayload{
+		ID: "1",
+	}
+	payloadBytes, _ := json.Marshal(housingPayload)
+
+	req, err := http.NewRequest("DELETE", "/api/housing/delete", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeleteHousingHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("expected status %d, got %d", http.StatusOK, status)
+	}
+
+	_, exists := data.Housings["1"]
+	if exists {
+		t.Errorf("housing data was not deleted.")
+	}
 }
