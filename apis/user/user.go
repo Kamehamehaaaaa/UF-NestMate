e40f4263b70efd3c685af000b35b3a1be52b5c03
Lakksh Tyagi <lakksh.tyagi22@gmail.com>
2025-04-15 14:59:21 -0400

    preferences struct added

--- apis/user/user.go
+++ apis/user/user.go
@@ -1,11 +1,11 @@
 package user
 
 type UserPayload struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	UserName  string `json:"username"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
+	FirstName   string       `json:"firstname"`
+	LastName    string       `json:"lastname"`
+	UserName    string       `json:"username"`
+	Password    string       `json:"password"`
+	Email       string       `json:"email"`
 	Favorites   []int        `json:"favorites" bson:"favorites"`
 	Preferences *Preferences `json:"preferences,omitempty" bson:"preferences,omitempty"`
 }
@@ -25,20 +25,18 @@ type User struct {
 }
 
 type Preferences struct {
-	Budget      Budget `json:"budget" bson:"budget"`
-	Smoking     string `json:"smoking" bson:"smoking"`
-	Cleanliness int    `json:"cleanliness" bson:"cleanliness"`
+	Budget           Budget `json:"budget" bson:"budget"`
+	Major            string `json:"major" bson:"major"`
+	Hobbies          string `json:"hobbies" bson:"hobbies"`
+	Food             string `json:"food" bson:"food"`
+	SleepingHabit    string `json:"sleeping_habit" bson:"sleeping_habit"`
+	SmokingDrinking  string `json:"smoking" bson:"smoking"`
+	Cleanliness      int    `json:"cleanliness" bson:"cleanliness"`
+	GenderPreference string `json:"gender_preference" bson:"gender_preference"`
+	PetPreference    string `json:"pet_preference" bson:"pet_preference"`
 }
 
 type Budget struct {
 	Min int `json:"min" bson:"min"`
 	Max int `json:"max" bson:"max"`
 }
-// type User struct {
-// 	UserId    int64  `json:"userId"`
-// 	FirstName string `json:"firstName"`
-// 	LastName  string `json:"lastName"`
-// 	UserName  string `json:"username"`
-// 	Password  string `json:"password"`
-// 	Email     string `json:"email"`
-// }
